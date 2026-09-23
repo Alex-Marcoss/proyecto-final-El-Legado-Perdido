23184894f0fe0a9ddcfd204fa1f475a010c9ebd4
Alex-Marcoss <marcosalexia.et35.2026@gmail.com>
2026-09-23 16:36:40 -0300

    Add files via upload

---
+++ assets/7.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="7" tilewidth="32" tileheight="32" tilecount="102" columns="17">
+<image source="7.png" width="560" height="192"/>
+</tileset>
